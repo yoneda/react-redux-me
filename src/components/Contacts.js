5492873665342d0a603ec31767d492d06f2c0cba
yoneda <nsshowdream@gmail.com>
2018-12-24 20:02:15 +0900

    add Contacts Component

---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contacts = ({button, items}) => {
+  return(
+    <div>
+      {button}
+      <h2>Contacts</h2>
+      {items}
+    </div>
+  )
+}
+
+export default Contacts
--- src/containers/CountContainer.js
+++ src/containers/CountContainer.js
@@ -1,57 +1,68 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 import Buttons from "../components/Buttons";
 import Display from "../components/Display";
 
 import Projects from "../components/Projects";
-import OpenButton from "../components/OpenButton";
+import PlusButton from "../components/PlusButton";
 import ProjectItem from "../components/ProjectItem";
+
+import Contacts from "../components/Contacts";
+import ContactItem from "../components/ContactItem";
 
 import { getAddCount, getSubCount, getResetCount } from "../actions/actions.js";
 
 const CountContainer = ({ count, addCountHandler, subCountHandler, resetCountHandler }) => {
-
-  const renderOpenButton = () => {
-    return(
-      <OpenButton isShow={true} handler={()=>console.log("aa")} />
-    )
-  }
-  const renderProjectItems = () => {
-    return(
-      <ProjectItem title={"title"} link={"yoneda.xyz"}/>
-    )
-  }
-  return(
+  const projects = [
+    {title:"kakeibo", link:"http://google.com"},
+    {title:"2 posts in a week", link:"http://google.com"}
+  ]
+  const contacts = [
+    {title:"kakeibo", link:"http://google.com"},
+    {title:"2 posts in a week", link:"http://google.com"}
+  ]
+  return (
     <div>
       <Display count={count} />
-      <Buttons addCountHandler={addCountHandler} subCountHandler={subCountHandler} resetCountHandler={resetCountHandler}/>
+      <Buttons
+        addCountHandler={addCountHandler}
+        subCountHandler={subCountHandler}
+        resetCountHandler={resetCountHandler}
+      />
       <Projects
-      button={
-        <OpenButton isShow={true} handler={()=>console.log("aa")} />
-      }
-      items={
-      <ProjectItem title={"title"} link={"yoneda.xyz"}/>
-      }
+        button={<PlusButton isShow={true} handler={() => console.log("aa")} />}
+        items={projects.map((project,index)=>{
+          return(<ProjectItem key={index} title={project.title} link={project.link} />)
+        })}
+      />
+      <Contacts
+        button={<PlusButton isShow={true} handler={() => console.log("aa")} />}
+        items={projects.map((project,index)=>{
+          return(<ProjectItem key={index} title={project.title} link={project.link} />)
+        })}
       />
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => {
-  return{
+  return {
     count: state.count
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   const addCountAction = getAddCount(1);
   const subCountAction = getSubCount(1);
   const resetCountAction = getResetCount();
-  return{
-    addCountHandler : (num) => dispatch(addCountAction),
-    subCountHandler : (num) => dispatch(subCountAction),
-    resetCountHandler : (num) => dispatch(resetCountAction)
-  }
-}
+  return {
+    addCountHandler: num => dispatch(addCountAction),
+    subCountHandler: num => dispatch(subCountAction),
+    resetCountHandler: num => dispatch(resetCountAction)
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(CountContainer)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CountContainer);
