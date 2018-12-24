b42e3ae6f89a5f85ff697e9e63df25697ee85eba
yoneda <nsshowdream@gmail.com>
2018-12-24 21:50:40 +0900

    add Posts Component

---
+++ src/components/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Posts = ({button}) => {
+  return(
+    <div>
+      {button}
+      <h2>Posts</h2>
+      <p>aaa</p>
+      <p>bbb</p>
+      <p>ccc</p>
+    </div>
+  )
+}
+
+export default Posts
