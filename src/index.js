eaaa96ed28bbd3bfb99b7e83f63931628642926e
yoneda <nsshowdream@gmail.com>
2018-12-29 00:03:36 +0900

    add webpack setting

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { Provider } from 'react-redux'
 const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("app")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
 )
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+  entry: "./src/index.js", // 出力元
+  output:{
+    path: `${__dirname}/public`, // 出力先
+    filename: "main.js",
+  },
+  mode: "development",
+  /* mode: "production", */
+  devServer:{
+    contentBase: "./public",
+    port: "3000"
+  },
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+        loaders: ["style-loader","css-loader?modules"],
+      },
+      {
+        test:/\.js/,
+        exclude:/node_modules/,
+        use:["babel-loader"]
+      },
+    ]
+  },
+};
