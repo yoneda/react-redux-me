ea2733c7e50d7a939cc0e2d47a1a233ff870d592
yoneda <nsshowdream@gmail.com>
2020-01-17 16:47:53 +0900

    [fix] make it possible to start app

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -61,11 +61,6 @@ const MainContainer = ({
         <div style={{ width: "760px", padding: "20px 20px 20px 20px" }}>
           <Background isDarkmode={dark} />
           {moonbuttonPressed === true ? <UpdateGrobalCss isDarkmode={dark} /> : ""}
-          
-          {/*アニメーションのテスト */}
-          <PlusButtonAnimated
-            updateHandler={(isOpen)=>console.log(isOpen)}
-          />
 
           <MoonButton
             isDarkmode={dark}
