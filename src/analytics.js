63a34a9cbd30a90c0db9a435acc30e4c53b94ecf
yoneda <nsshowdream@gmail.com>
2021-03-12 19:41:56 +0900

    [fix] escape some bugs related to umami

--- src/analytics.js
+++ src/analytics.js
@@ -1,31 +1,31 @@
 export function twitterClicked(){
-  return window.umami("twitter-clicked");
+  return window.hasOwnProperty("umami") && window.umami("twitter-clicked");
 }
 
 export function instagramClicked(){
-  return window.umami("instagram-clicked");
+  return window.hasOwnProperty("umami") && window.umami("instagram-clicked");
 }
 
 export function githubClicked(){
-  return window.umami("github-clicked");
+  return window.hasOwnProperty("umami") && window.umami("github-clicked");
 }
 
 export function behanceClicked(){
-  return window.umami("behance-clicked");
+  return window.hasOwnProperty("umami") && window.umami("behance-clicked");
 }
 
 export function darkmodeOn(){
-  return window.umami("darkmode-on");
+  return window.hasOwnProperty("umami") && window.umami("darkmode-on");
 }
 
 export function projectsOpen(){
-  return window.umami("projects-open");
+  return window.hasOwnProperty("umami") && window.umami("projects-open");
 }
 
 export function contactsOpen(){
-  return window.umami("contacts-open");
+  return window.hasOwnProperty("umami") && window.umami("contacts-open");
 }
 
 export function postsOpen(){
-  return window.umami("posts-open");
+  return window.hasOwnProperty("umami") && window.umami("posts-open");
 }
